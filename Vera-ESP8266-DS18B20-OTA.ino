c5a574ce49c3a24e1276b10150dc691907704315
Kari Tiirikainen <kari.tiirikainen@gmail.com>
2021-06-13 15:23:17 +0300

    Remove Deepsleep workaround, it still causes constant ~2mA current flow and renders the ESP useless for battery based operation. Only proper fix is to acquire ESP8266s that do not have the DeepSleep bug.
    Add temperature memory to prolong Deepsleep interval in case there is only minor change in temperature to further conserve battery.

--- Vera-ESP8266-DS18B20-OTA.ino
+++ Vera-ESP8266-DS18B20-OTA.ino
@@ -30,7 +30,8 @@ struct {
   uint32_t crc32=0;   // 4 bytes
   uint8_t channel=0;  // 1 byte,   5 in total
   uint8_t bssid[6]={0}; // 6 bytes, 11 in total
-  uint8_t padding=0;  // 1 byte,  12 in total
+  float old_temp=0;   // 4 bytes, 15
+  uint8_t padding=0;  // 1 byte,  16 in total
 } rtcData;
 
 //Read battery voltage
@@ -47,32 +48,16 @@ long glb_rssi=0;
 WiFiClient client;
 HTTPClient http;
 
-#define ets_wdt_disable ((void (*)(void))0x400030f0)
-#define ets_delay_us ((void (*)(int))0x40002ecc)
-#define _R (uint32_t *)0x60000700
-
-void GoToDeepSleep(int SleepDelay){
+void gotoDeepSleep(int SleepDelay){
   Serial.print("Sleeping " + String(SleepDelay) + " seconds\n");
-  uint64_t time = SleepDelay * 1000000;
-  //DeepSleep workaround for broken ESP8266 boards
-  //https://github.com/Erriez/ErriezArduinoExamples/commit/486c9b9a15ef3721e83206f561633e832e6649c6
+  ESP.deepSleepInstant(SleepDelay * 1000000,WAKE_RF_DISABLED);
+}
 
-  delay(10);
-
-   ets_wdt_disable();
-   *(_R + 4) = 0;
-   *(_R + 17) = 4;
-   *(_R + 1) = *(_R + 7) + 5;
-   *(_R + 6) = 8;
-   *(_R + 2) = 1 << 20;
-   ets_delay_us(10);
-   *(_R + 39) = 0x11;
-   *(_R + 40) = 3;
-   *(_R) &= 0xFCF;
-   *(_R + 1) = *(_R + 7) + (45*(time >> 8));
-   *(_R + 16) = 0x7F;
-   *(_R + 2) = 1 << 20;
-   __asm volatile ("waiti 0");
+//Update temperature reading into RTC memory
+void updateOldTemp () {
+   rtcData.old_temp = glb_temp;
+   rtcData.crc32 = crc32( ((uint8_t*)&rtcData) + 4, sizeof( rtcData ) - 4,0xffffffff );
+   ESP.rtcUserMemoryWrite( 0, (uint32_t*)&rtcData, sizeof( rtcData ) );
 }
 
 void update_started() {
@@ -104,7 +89,7 @@ int checkForUpdates() {
       Serial.print("\nBattery Level too low for OTA update\n");
       return 1;
     }
-
+    
     Serial.print("\nChecking for update from " + String(UpdateURL) + ". Current FW version " + String(FWVersion) + "\n");
     t_httpUpdate_return ret = ESPhttpUpdate.update(client, String(UpdateURL),String(FWVersion));
 
@@ -155,7 +140,7 @@ int connect_wifi (){
   else {
     // The RTC data was not valid, so make a regular connection
     Serial.printf("[WIFI] ... normal connect, status: %d\n", wifiStatus);
-    WiFi.begin( WifiSSID, WifiPass );
+    WiFi.begin( WifiSSID, WifiPass, WifiChannel, WifiBSSID, true );
   }
 
   while( wifiStatus != WL_CONNECTED ) {
@@ -176,7 +161,7 @@ int connect_wifi (){
       WiFi.disconnect( true );
       delay( 1 );
       WiFi.mode( WIFI_OFF );
-      GoToDeepSleep(sleepTimeS);
+      gotoDeepSleep(sleepTimeS);
       return 1; // Not expecting this to be called, the previous call will never return.
     }
     delay( 50 );
@@ -200,9 +185,12 @@ return wifiStatus;
 int ReadSensor() {
   int readloops=0;
 
-  // Set the resolution for all devices to 10 bits = 0.25C :
+  //Initialize sensor
+  DS18B20.begin();
+  
+  // Set the resolution for all devices to 10 bits = 0.25C
   DS18B20.setResolution(10);
-
+  
   Serial.print("\nRequesting DS18B20 temperature...\n");
 
   do {
@@ -212,6 +200,7 @@ int ReadSensor() {
 
     readloops++;
   } while (glb_temp == 85.0 || glb_temp == (-127.0) || readloops <=5);
+
 
   // Check if any reads failed and exit early (to try again).
   if (isnan(glb_temp)) {
@@ -225,41 +214,45 @@ int ReadSensor() {
 
 float getBatteryStatus(){
   glb_batterylevel=ESP.getVcc();
-
+    
   Serial.println("Battery VCC: " + String(glb_batterylevel*0.001)+ "V");
 
   //Calculate estimated percentage
-  if (glb_batterylevel >= 3400) {
+  if (glb_batterylevel >= 3800) {
     glb_batterylevel = 100;
     }
-  else if ( glb_batterylevel <= 3400 || glb_batterylevel >= 3300 ) {
+  else if ( glb_batterylevel <= 3799 || glb_batterylevel >= 3600 ) {
     glb_batterylevel = 50;
     }
-  else {
+  else if ( glb_batterylevel <= 3599 || glb_batterylevel >= 3100 ) {
     glb_batterylevel = 10;
+    }
+  else {
+    //Battery too low (<3100), sleep 6h or forever which ever comes first.
+    gotoDeepSleep(21600);
     }
 
   Serial.println("Battery level: " + String(glb_batterylevel) + "%");
-
+    
   return 0; 
 }
 
 int getRSSI(){
   glb_rssi=WiFi.RSSI();
-
+    
   Serial.println("WiFi RSSI: " + String(glb_rssi) + "dBm");
-
-  return 0;
+   
+  return 0; 
 }
 
 int GetHttpURL(String MyURL){
   Serial.print("[HTTP] begin...\n");
 
   http.begin(client,String(VeraBaseURL) + MyURL);
-
+    
   int httpCode = http.GET();
 
-  if (httpCode > 0) {
+   if (httpCode > 0) {
      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
     
      if (httpCode == HTTP_CODE_OK) {
@@ -274,35 +267,23 @@ int GetHttpURL(String MyURL){
 }
 
 void setup() {
+
   //switch radio off to save energy
   WiFi.mode(WIFI_OFF);
   WiFi.forceSleepBegin();
 
-  //Disable WiFi status LED to save battery
-  wifi_status_led_uninstall();
-
-  delay(100);
-
   Serial.begin(115200);
   Serial.setDebugOutput(false);
-
-  // Debug info
-  // rst_info *rinfo = ESP.getResetInfoPtr();
-  // Serial.print(String("\nResetInfo.reason = ") + (*rinfo).reason + ": " + ESP.getResetReason() + "\n");
-
-  //Initialize sensor
-  DS18B20.begin();
-
   Serial.print("\n" + String(ESPName) + " started\n");
+
+  getBatteryStatus();
 
   int SensorStatus = ReadSensor(); 
 
   if (SensorStatus > 0) {
-    GoToDeepSleep(sleepTimeS);
+    gotoDeepSleep(sleepTimeS);
   }
-
-  getBatteryStatus();
-
+  
   // Connect WiFi
   connect_wifi();
 
@@ -316,7 +297,17 @@ void setup() {
 
   http.end();
 
-  GoToDeepSleep(sleepTimeS);
+  //switch radio off to save energy
+  WiFi.mode(WIFI_OFF);
+
+  //Sleep longer if level change is small, there is very little change during night.
+  if (abs(abs(glb_temp) - abs(rtcData.old_temp)) >= tempThreshold) {
+    updateOldTemp();
+    gotoDeepSleep(sleepTimeS);
+  }
+  else {
+    gotoDeepSleep(sleepTimeL);
+  }
 }
 
 void loop() {
--- config.h
+++ config.h
@@ -6,10 +6,14 @@ IPAddress subnet(255,255,255,0);
 
 const char* WifiSSID      = "SSID";
 const char* WifiPass      = "PASSWORD"; 
-const int sleepTimeS      = 300; // Sleep time in seconds
+uint8_t WifiBSSID[6]      = {0x30,0xD3,0x2D,0x1F,0xF2,0x69};
+uint8_t WifiChannel       = 11;
+const int sleepTimeS      = 600; // Sleep time in seconds
+const int sleepTimeL      = 1800; // Sleep time in seconds
+const float tempThreshold  = 1.0; // A change greater than levelThreshold will trigger sleepTimeS
 
 int VeraTempDeviceID = 146;
 const char* ESPName = "Vera-DS18B20";
 const char* VeraBaseURL = "http://192.168.1.42:3480/";
 const char* UpdateURL = "http://192.168.1.254:4080/8266OTA.php";
-const char* FWVersion = "28112020";
+const char* FWVersion = "06052021";
