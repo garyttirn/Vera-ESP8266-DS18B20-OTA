651d19651a24b320c4ead1cbae9410e61b339ae4
Kari Tiirikainen <kari.tiirikainen@gmail.com>
2021-07-22 18:16:58 +0300

    tune battery voltage tresholds and cleanup code
     Changes to be committed:
    	modified:   Vera-ESP8266-DS18B20-OTA.ino

--- Vera-ESP8266-DS18B20-OTA.ino
+++ Vera-ESP8266-DS18B20-OTA.ino
@@ -39,7 +39,7 @@ ADC_MODE(ADC_VCC);
 
 #include "config.h"
 
-String ProgramVersion  = "0.1";
+String ProgramVersion  = "0.2";
 
 float glb_temp=0;
 int glb_batterylevel=0;
@@ -48,7 +48,7 @@ long glb_rssi=0;
 WiFiClient client;
 HTTPClient http;
 
-void gotoDeepSleep(int SleepDelay){
+void gotoDeepSleep(int SleepDelay) {
   Serial.print("Sleeping " + String(SleepDelay) + " seconds\n");
   ESP.deepSleepInstant(SleepDelay * 1000000,WAKE_RF_DISABLED);
 }
@@ -77,36 +77,36 @@ void update_error(int err) {
 }
 
 int checkForUpdates() {
-    ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW);
+  ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW);
 
-    // Add optional callback notifiers
-    ESPhttpUpdate.onStart(update_started);
-    ESPhttpUpdate.onEnd(update_finished);
-    ESPhttpUpdate.onProgress(update_progress);
-    ESPhttpUpdate.onError(update_error);
+  // Add optional callback notifiers
+  ESPhttpUpdate.onStart(update_started);
+  ESPhttpUpdate.onEnd(update_finished);
+  ESPhttpUpdate.onProgress(update_progress);
+  ESPhttpUpdate.onError(update_error);
 
-    if (glb_batterylevel < 50) {
-      Serial.print("\nBattery Level too low for OTA update\n");
-      return 1;
-    }
+  if (glb_batterylevel < 50) {
+    Serial.print("\nBattery Level too low for OTA update\n");
+    return 1;
+   }
     
-    Serial.print("\nChecking for update from " + String(UpdateURL) + ". Current FW version " + String(FWVersion) + "\n");
-    t_httpUpdate_return ret = ESPhttpUpdate.update(client, String(UpdateURL),String(FWVersion));
+  Serial.print("\nChecking for update from " + String(UpdateURL) + ". Current FW version " + String(FWVersion) + "\n");
+  t_httpUpdate_return ret = ESPhttpUpdate.update(client, String(UpdateURL),String(FWVersion));
 
-    switch (ret) {
-      case HTTP_UPDATE_FAILED:
-        Serial.printf("HTTP_UPDATE_FAILED Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
-        break;
+  switch (ret) {
+    case HTTP_UPDATE_FAILED:
+      Serial.printf("HTTP_UPDATE_FAILED Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+      break;
 
-      case HTTP_UPDATE_NO_UPDATES:
-        Serial.println("HTTP_UPDATE_NO_UPDATES");
-        break;
+    case HTTP_UPDATE_NO_UPDATES:
+      Serial.println("HTTP_UPDATE_NO_UPDATES");
+      break;
 
-      case HTTP_UPDATE_OK:
-        Serial.println("HTTP_UPDATE_OK");
-        delay(5000);
-        ESP.restart();
-        break;
+    case HTTP_UPDATE_OK:
+      Serial.println("HTTP_UPDATE_OK");
+      delay(5000);
+      ESP.restart();
+      break;
     }
 
 return 0;
@@ -124,15 +124,15 @@ int connect_wifi (){
 
   // Try to read WiFi settings from RTC memory
   bool rtcValid = false;
-  if( ESP.rtcUserMemoryRead( 0, (uint32_t*)&rtcData, sizeof( rtcData ) ) ) {
+  if(ESP.rtcUserMemoryRead(0,(uint32_t*)&rtcData,sizeof(rtcData))) {
     // Calculate the CRC of what we just read from RTC memory, but skip the first 4 bytes as that's the checksum itself.
-    uint32_t crc = crc32( ((uint8_t*)&rtcData) + 4, sizeof( rtcData ) - 4,0xffffffff);
-    if( crc == rtcData.crc32 ) {
+    uint32_t crc = crc32(((uint8_t*)&rtcData) + 4, sizeof(rtcData) - 4,0xffffffff);
+    if(crc == rtcData.crc32) {
       rtcValid = true;
     }
   }
 
-  if( rtcValid ) {
+  if(rtcValid) {
     // The RTC data was good, make a quick connection
     Serial.printf("[WIFI] ... quick connect, status: %d\n", wifiStatus);
     WiFi.begin( WifiSSID, WifiPass, rtcData.channel, rtcData.bssid, true );
@@ -145,7 +145,7 @@ int connect_wifi (){
 
   while( wifiStatus != WL_CONNECTED ) {
     retries++;
-     if( retries == 100 ) {
+     if(retries == 100) {
        // Quick connect is not working, reset WiFi and try regular connection
        rtcValid = false;
        WiFi.disconnect();
@@ -156,28 +156,28 @@ int connect_wifi (){
        delay( 10 );
        WiFi.begin( WifiSSID, WifiPass );
     }
-    if( retries >= 600 ) {
+    if(retries >= 600) {
       // Giving up after 30 seconds and going back to sleep
       WiFi.disconnect( true );
-      delay( 1 );
-      WiFi.mode( WIFI_OFF );
+      delay(1);
+      WiFi.mode(WIFI_OFF);
       gotoDeepSleep(sleepTimeS);
       return 1; // Not expecting this to be called, the previous call will never return.
     }
-    delay( 50 );
+    delay(50);
     wifiStatus = WiFi.status();
   }
 
-  if( rtcValid == false ) {
+  if(rtcValid == false) {
     // Write current connection info back to RTC
-   rtcData.channel = WiFi.channel();
-   memcpy( rtcData.bssid, WiFi.BSSID(), 6 ); // Copy 6 bytes of BSSID (AP's MAC address)
-   rtcData.crc32 = crc32( ((uint8_t*)&rtcData) + 4, sizeof( rtcData ) - 4,0xffffffff );
-   ESP.rtcUserMemoryWrite( 0, (uint32_t*)&rtcData, sizeof( rtcData ) );
+    rtcData.channel = WiFi.channel();
+    memcpy( rtcData.bssid, WiFi.BSSID(), 6 ); // Copy 6 bytes of BSSID (AP's MAC address)
+    rtcData.crc32 = crc32( ((uint8_t*)&rtcData) + 4, sizeof( rtcData ) - 4,0xffffffff );
+    ESP.rtcUserMemoryWrite( 0, (uint32_t*)&rtcData, sizeof( rtcData ) );
 
-   //Check for updates
-   checkForUpdates();
- }
+    //Check for updates
+    checkForUpdates();
+  }
 
 return wifiStatus;
 }
@@ -224,11 +224,11 @@ int getBatteryStatus(){
   else if ( glb_batterylevel <= 3799 || glb_batterylevel >= 3600 ) {
     glb_batterylevel = 50;
     }
-  else if ( glb_batterylevel <= 3599 || glb_batterylevel >= 3100 ) {
+  else if ( glb_batterylevel <= 3599 || glb_batterylevel >= 3300 ) {
     glb_batterylevel = 10;
     }
   else {
-    //Battery too low (<3100), sleep 6h or forever which ever comes first.
+    //Battery too low (<3300), sleep 6h or forever which ever comes first.
     gotoDeepSleep(21600);
     }
 
@@ -252,7 +252,7 @@ int GetHttpURL(String MyURL){
     
   int httpCode = http.GET();
 
-   if (httpCode > 0) {
+  if (httpCode > 0) {
      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
     
      if (httpCode == HTTP_CODE_OK) {
